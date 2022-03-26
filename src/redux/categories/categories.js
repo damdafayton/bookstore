e17b2bbe3b80da7ad37b1d3e53e102a0b9debb9e
backtofayton <yucelolcay@gmail.com>
2022-03-26 21:42:03 +0200

    Update name

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -2,19 +2,18 @@ const UNDER_CONSTRUCTION = 'categories/under_construction'
 
 export const underConstruction = () => ({ type: UNDER_CONSTRUCTION })
 
-const categoriesUnderConstruction = 'Under Construction'
+const underConstructionStr = 'Under Construction'
 const initialState = []
 
 export default function categoriesReducer(state = initialState, action) {
   switch (action.type) {
     case UNDER_CONSTRUCTION:
-      return categoriesUnderConstruction;
+      return underConstructionStr;
     default:
       return state
   }
 }
 
-
 test('if categories/under_construction action works', () => {
-  expect(categoriesReducer(initialState, underConstruction())).toBe(categoriesUnderConstruction)
+  expect(categoriesReducer(initialState, underConstruction())).toBe(underConstructionStr)
 })
